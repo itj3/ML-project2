af1ed221a81534b51a41db9342bf480454493d47
ktaed <ktaed@nau.edu>
2015-05-05 11:27:38 -0700

    Gen_plot will plot all the nodes in the dataset time frame

--- gen_plot.py
+++ gen_plot.py
@@ -8,20 +8,16 @@ import room
 from model import *
 
 temp_data = file_reader.read('night.csv')
-date = []
-outTemp = []
-inTemp = []
-
 rooms = []
+
 for x in temp_data:
     temp_room = room.Room(x)
     for y in temp_data[x]:
         temp_room.add_temp(y)
     rooms.append(temp_room)
 
-
 start= datetime.datetime(2015,4,10,22,00)
-end = datetime.datetime(2015,4,11,7,0)
+end = datetime.datetime(2015,4,11,7,30)
 timeFrame = end-start
 zero = datetime.timedelta(0)
 labels = {}
@@ -32,43 +28,30 @@ labels['17'] = 'c.'
 labels['18'] = 'g.'
 labels['19'] = 'k.'
 i=10
+
+datetime.datetime(2015,4,11,1,13)
 try:
     for r in rooms:
-        if r.room_num == '10':
-            temp = r
-
-    for y in temp_data[str(i)]:
-        if ( end - y[0]<= timeFrame and  (end - y[0])>=zero):
-            date.append(y[0])
-            inTemp.append(y[1])
-            outTemp.append(y[2])
+        date =[]
+        inTemp = []
+        outTemp =[]
+        line = labels[str(r.room_num)]
+        for y in r.temps:
+            if ( end - y[0]<= timeFrame and  (end - y[0])>=zero):
+                date.append(y[0])
+                inTemp.append(y[1])
+                outTemp.append(y[2])
+        plt.plot(date,inTemp, "%s"%(line), label="Sensor #%s" %(r.room_num))
 except KeyError:
     pass
-# print infer_vars(temp)
-tau = all_tau(temp)
-a, =plt.plot(date,inTemp, "b.", label='Sensor 10/Rm #164 Temp')
-b, =plt.plot(date, outTemp, "m.", label="Outside Temp")
-
-# plt.legend()
-
 plt.ylabel("Temperature ($^\circ$C)")
 plt.xlabel("4/10 to 4/11")
 plt.xticks(rotation='45')
-plt.gca().add_artist(plt.legend(handles=[a], loc=1))
-plt.legend(handles=[b], loc=4)
 plt.title("Temperature Vs. Time", fontsize=20)
-plt.tight_layout()
-plt.savefig('TempVsTime.png')
-plt.cla()
 
-b, = plt.plot(tau[1],tau[0], "k.", label="Tau")
-plt.ylabel("Tau")
 plt.xlabel("4/10 to 4/11")
-plt.xticks(rotation='45')
-
-plt.legend(handles=[b], loc=2)
-plt.title("Tau Vs. Time", fontsize=20)
+plt.legend(loc=8, handletextpad=0,handlelength=0.5,columnspacing=0.5,numpoints=1,fancybox=True, ncol=6,fontsize=10)
 plt.tight_layout()
-plt.savefig('Temp_Tau.png')
+plt.savefig('All_Sens.png')
 
 plt.show()
