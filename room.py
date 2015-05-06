53ace866324186da802c984454080c1684f7c1a2
itj3 <itj3@nau.edu>
2015-05-06 13:07:01 -0700

    got rid of useless stuff in room.py

--- room.py
+++ room.py
@@ -2,23 +2,12 @@ from datetime import datetime as dt
 
 class Room():
 	def __init__(self, room_num):
-		self.room_num = room_num # room number
-		self.window_sa = 0 # surface area of window space
-
-		self.temps = [] # 2D list containing temperatures and time EX: [[datetime object,65], [datetime object,66]]
-
-	def get_room_num(self):
-		return self.room_num
-
-	def get_window_sa(self):
-		return self.window_sa
+		self.room_num = room_num # acctually is the node number
+		self.temps = [] # 2D list containing temperatures and time EX: [[datetime object, inside temp, outside temp], [datetime object,66,34]]
 
 	def get_temps(self, date): #date is a datetime object 
 		return self.temps
 
 	def add_temp(self, data):
 		self.temps.append(data)
-
-	def set_SA(self, SA):
-		self.window_SA = SA
 
