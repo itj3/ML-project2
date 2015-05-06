9e0ded8799ce44445e98b138c738d7bb8dcd1583
itj3 <itj3@nau.edu>
2015-05-06 13:07:54 -0700

    main now also prints building tau

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import file_reader
 import room
 from model import infer_tau, update_heat
-from output import plot_model, print_tau
+from output import plot_model, print_tau, print_b_tau
 
 
 def main():
@@ -10,11 +10,17 @@ def main():
     print 'Data loading complete'
     # build a list of room objects
     rooms = []
+    building_rooms = []
     for x in temp_data:
         temp_room = room.Room(x)
         for y in temp_data[x]:
             temp_room.add_temp(y)
         rooms.append(temp_room)
+        if temp_room.room_num == '7' or temp_room.room_num == '9' or temp_room.room_num == '10' or temp_room.room_num == '19':
+            building_rooms.append(temp_room)
+
+
+
 
     print 'Room construction complete'
     # for each room, calculate and print tau, and plot the resulting model
@@ -36,8 +42,11 @@ def main():
             actual_heat_values.append(temp[1])
 
         print_tau(r.room_num, tau)
-        # IF MATPLOTLIB NOT INSTALLED, COMMENT OUT THIS LINE
-        plot_model(r.room_num, time_steps, calculated_heat_values, actual_heat_values)
+        # IF MATPLOTLIB NOT INSTALLED, COMMENT OUT line below
+        # plot_model(r.room_num, time_steps, calculated_heat_values, actual_heat_values)
+    # print tau for the building
+    print_b_tau(building_rooms) 
+
 
 
 if __name__ == "__main__":
